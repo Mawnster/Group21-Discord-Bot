92d86dfe8d03dd02b47079ff2157a749c2173f1c
Zach <zachary.bricknell@gmail.com>
2022-10-18 03:05:53 -0400

    Finished the web scraper to get the required PDF. Started a script to extract data from the PDF, Still incomplete

--- WebScraper/TrentuScraper.py
+++ WebScraper/TrentuScraper.py
@@ -1,28 +1,43 @@
-import requests
-from bs4 import BeautifulSoup
+#To pause for testing
+import os
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-#The URL we are looking to scrap
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+#Gets cwd so that on any machine it will download the file directly to 
+download_dir = os.getcwd() + "\\TrentUAC"
 
-#required by the library 
-soup = BeautifulSoup(page.content, "html.parser")
+#changing the options to allow for chrome to download the file to the CWD instead of opening it
+#adapted from https://stackoverflow.com/questions/53998690/downloading-a-pdf-using-selenium-chrome-and-python
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_experimental_option('prefs', {"plugins.always_open_pdf_externally": True,
+                                                "download.default_directory": download_dir,
+                                                "download.prompt_for_download": False,
+                                                "download.directory_upgrade": True,})
 
-#searching only the main container on the website
-results = soup.find(id="ResultsContainer")
+#Instead of having a full chrome install we can use this to launch come
+#append the defined options to chrome when opening
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chromeOptions)
 
-#get all the div tags labeled card-content
-job_elements = results.find_all("div", class_="card-content")
+#Static URL of the web page we are scraping, This shouldn't ever change
+url = 'https://www.trentu.ca/registrar/academic-calendar/undergraduate-calendar'
 
-#Iterate over all the results and extract just the title location and company
-#Then only output the text, and strip the whitespace
-for job_element in job_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    print()
-    
-    
+#Launches the url and gives a few seconds to process. 
+driver.get(url)
+time.sleep(3)
+#using the xpath even when the PDF gets updated the button will still lead to the new one every time
+driver.find_element('xpath', '//*[@id="block-system-main"]/div/div/div/div/p[1]/a').click()
+time.sleep(6)
+
+
+#this will delete the old instance of the file and rename the new one appropriately
+PDF_Directory = os.getcwd() + "\\TrentUAC"
+
+for file in os.listdir(PDF_Directory):
+    if "UpdatedUAC.pdf" in file:
+        os.remove(PDF_Directory + "\\" + file)
+
+for file in os.listdir(PDF_Directory):
+    if "Undergrad" in file:
+        os.rename(PDF_Directory + "\\" + file, PDF_Directory + "\\" + "TrentUAC.pdf")
+            
